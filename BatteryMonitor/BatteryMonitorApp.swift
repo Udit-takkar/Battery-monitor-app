e4d398f2909204d18af5fd61d12a50fed446d0ab
Udit Takkar <udit222001@gmail.com>
2025-01-06 02:23:10 +0530

    Initial Commit

---
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryMonitorApp.swift
+//  BatteryMonitor
+//
+//  Created by Udit Takkar on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryMonitorTests/BatteryMonitorTests.swift
@@ -0,0 +1,16 @@
+//
+//  BatteryMonitorTests.swift
+//  BatteryMonitorTests
+//
+//  Created by Udit Takkar on 06/01/25.
+//
+
+import Testing
+
+struct BatteryMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
